0f0dacc1bee5e4c5e2ac6e8ebb4ccd4201f85a5d
Lucas Kikkawa <45891312+lucaskikkawa@users.noreply.github.com>
2022-05-02 10:43:49 -0300

    v0.3 add validation input

---
+++ src/com/company/Converter.java
@@ -0,0 +1,101 @@
+package com.company;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class Converter
+{
+    public static void convert(String currency_from, String currency_to, double amount) throws IOException
+    {
+        double exchangeRate;
+        String currency_from_name = "";
+        String currency_to_name = "";
+        String currency_from_symbol = "";
+        String currency_to_symbol = "";
+
+
+        String API_ACCESS_KEY = "ef70f000b4a187533570";
+
+        String GET_URL_CURRENCY = "https://free.currconv.com/api/v7/currencies" +
+                "?apiKey=" + API_ACCESS_KEY;
+
+        String GET_URL_CONVERT = "https://free.currconv.com/api/v7/convert" +
+                "?apiKey=" + API_ACCESS_KEY +
+                "&q=" + currency_from + "_" + currency_to +
+                "&compact=ultra";
+
+        // URL connection to get both currency_name and currency_symbol
+
+        URL url_name_symbol = new URL(GET_URL_CURRENCY);
+
+        HttpURLConnection name_symbol_httpURLConnection = (HttpURLConnection) url_name_symbol.openConnection();
+        name_symbol_httpURLConnection.setRequestMethod("GET"); // requesting data
+        int name_symbol_responseCode = name_symbol_httpURLConnection.getResponseCode();
+
+        if (name_symbol_responseCode == HttpURLConnection.HTTP_OK)
+        {
+            BufferedReader in = new BufferedReader(new InputStreamReader(name_symbol_httpURLConnection.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null)
+            {
+                response.append(inputLine);
+            }in.close();
+
+            JSONObject obj = new JSONObject(response.toString());
+            currency_from_name= obj.getJSONObject("results").getJSONObject(currency_from).getString("currencyName");
+            currency_from_symbol= obj.getJSONObject("results").getJSONObject(currency_from).getString("currencySymbol");
+            currency_to_name= obj.getJSONObject("results").getJSONObject(currency_to).getString("currencyName");
+            currency_to_symbol= obj.getJSONObject("results").getJSONObject(currency_to).getString("currencySymbol");
+        }
+
+        // URL connection to get exchangeRate
+
+
+        URL url_convert = new URL(GET_URL_CONVERT);
+
+        HttpURLConnection exchangeRate_httpURLConnection = (HttpURLConnection) url_convert.openConnection();
+        exchangeRate_httpURLConnection.setRequestMethod("GET"); // requesting data
+        int responseCode = exchangeRate_httpURLConnection.getResponseCode();
+
+        if (responseCode == HttpURLConnection.HTTP_OK) // success stabilishing connection
+        {
+            BufferedReader in = new BufferedReader(new InputStreamReader(exchangeRate_httpURLConnection.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+
+            // add requested data to buffer
+            while ((inputLine = in.readLine()) != null)
+            {
+                response.append(inputLine);
+            }in.close();
+
+            JSONObject obj = new JSONObject(response.toString());
+            exchangeRate = obj.getDouble(currency_from + "_" + currency_to);
+            //System.out.println(exchangeRate);
+
+            double converted_amount = amount * exchangeRate;
+
+            String convert_msg = String.format("Converting %s%.2f (%s) to %s (%s)... \n",
+                    currency_from_symbol,
+                    amount,
+                    currency_from_name,
+                    currency_to,
+                    currency_to_name);
+            String converted_msg = String.format("%s%.2f is equal to %s%.2f.",currency_from_symbol,amount,currency_to_symbol,converted_amount);
+
+            System.out.println(convert_msg);
+            System.out.println(converted_msg);
+
+        }
+
+    }
+
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,16 +1,12 @@
 package com.company;
 
-import org.json.JSONObject;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
+
+import static com.company.Converter.convert;
 
 public class Main {
 
@@ -19,6 +15,20 @@ public class Main {
     // then, it will return X * rates ( rate between currencies ).
 
     // API I'm using in this project: https://www.currencyconverterapi.com/
+
+
+    /*
+        02/05/2022
+
+        Create a while loop so the user can do multiple conversions
+        Check user inputs (String, doubles)
+        Uppercase all string inputs
+
+        To-do
+
+        validate amount input (check if it has comma (,) and 2 decimals after comma
+
+     */
 
     public static void main(String[] args) throws IOException
     {
@@ -37,10 +47,15 @@ public class Main {
         currencies.put("HKD","Hong Kong Dollar");
         currencies.put("KRW","South Korean Won");
 
-        String currency_from;
-        String currency_to;
-        double amount;
-
+        String currency_from = "";
+        String currency_to = "";
+        String exitInput;
+        String amountInput;
+        double amount = 0; // amount >= 0 && amount must have 2 decimal points
+        boolean exit = false;
+        boolean validateCurrencyFrom = false;
+        boolean validateCurrencyTo = false;
+        boolean validateAmount = false;
 
 
 
@@ -50,17 +65,70 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Please, type in which currency you want to convert FROM: ");
-        currency_from = sc.nextLine();
+        while(!exit)
+        {
+            while(!validateCurrencyFrom)
+            {
+                System.out.println("Please, type in which currency you want to convert FROM:");
+                currency_from = sc.nextLine().toUpperCase();
+                String check = currencies.get(currency_from);
+                if (check == null)
+                {
+                    System.out.println("Invalid input. Please type a valid currency.");
+                } else
+                {
+                    validateCurrencyFrom = true;
+                }
+            }
 
-        System.out.println("Please, type in which currency you want to convert TO:");
-        currency_to = sc.nextLine();
+            while(!validateCurrencyTo)
+            {
+                System.out.println("Please, type in which currency you want to convert TO:");
+                currency_to = sc.nextLine().toUpperCase();
+                String check = currencies.get(currency_to);
+                if(check == null)
+                {
+                    System.out.println("Invalid input. Please type a valid currency.");
+                } else
+                {
+                    validateCurrencyTo = true;
+                }
+            }
 
-        System.out.println("Please, insert the amount you want to convert: ");
-        amount = sc.nextDouble();
+            while(!validateAmount)
+            {
+                System.out.println("Please, insert the amount you want to convert using a comma (,) for cents : ");
+                amountInput = sc.nextLine();
+                amount = Double.parseDouble(amountInput);
+                if (amount <= 0.00)
+                {
+                    System.out.println("Invalid input. Please type a valid amount");
+                } else
+                {
+                    validateAmount = true;
+                }
 
-        converter(currency_from,currency_to,amount);
+            }
+            convert(currency_from, currency_to, amount);
+
+            System.out.println("Do you want to convert again? Type Y for Yes, N for no:");
+            exitInput = sc.nextLine().toUpperCase();
+
+            if (exitInput.equals("Y"))
+            {
+                continue;
+            } else if (exitInput.equals("N"))
+            {
+                exit = true;
+                sc.close();
+            }
+
+
+        }
+
+        System.out.println("Thanks for using my currency converter! See you soon!");
     }
+
 
     public static void availableCurrencies(Map<String,String> map)
     {
@@ -72,93 +140,6 @@ public class Main {
         System.out.println();
     }
 
-    private static void converter(String currency_from, String currency_to, double amount) throws IOException
-    {
-        double exchangeRate = 0;
-        String currency_from_name = "";
-        String currency_to_name = "";
-        String currency_from_symbol = "";
-        String currency_to_symbol = "";
-
-
-        String API_ACCESS_KEY = "ef70f000b4a187533570";
-
-        String GET_URL_CURRENCY = "https://free.currconv.com/api/v7/currencies" +
-                "?apiKey=" + API_ACCESS_KEY;
-
-        String GET_URL_CONVERT = "https://free.currconv.com/api/v7/convert" +
-                "?apiKey=" + API_ACCESS_KEY +
-                "&q=" + currency_from + "_" + currency_to +
-                "&compact=ultra";
-
-        // URL connection to get both currency_name and currency_symbol
-        
-        URL url_name_symbol = new URL(GET_URL_CURRENCY);
-
-        HttpURLConnection name_symbol_httpURLConnection = (HttpURLConnection) url_name_symbol.openConnection();
-        name_symbol_httpURLConnection.setRequestMethod("GET"); // requesting data
-        int name_symbol_responseCode = name_symbol_httpURLConnection.getResponseCode();
-
-        if (name_symbol_responseCode == HttpURLConnection.HTTP_OK)
-        {
-            BufferedReader in = new BufferedReader(new InputStreamReader(name_symbol_httpURLConnection.getInputStream()));
-            String inputLine;
-            StringBuffer response = new StringBuffer();
-
-            while ((inputLine = in.readLine()) != null)
-            {
-                response.append(inputLine);
-            }in.close();
-
-            JSONObject obj = new JSONObject(response.toString());
-            currency_from_name= obj.getJSONObject("results").getJSONObject(currency_from).getString("currencyName");
-            currency_from_symbol= obj.getJSONObject("results").getJSONObject(currency_from).getString("currencySymbol");
-            currency_to_name= obj.getJSONObject("results").getJSONObject(currency_to).getString("currencyName");
-            currency_to_symbol= obj.getJSONObject("results").getJSONObject(currency_to).getString("currencySymbol");
-        }
-
-        // URL connection to get exchangeRate
-
-
-        URL url_convert = new URL(GET_URL_CONVERT);
-
-        HttpURLConnection exchangeRate_httpURLConnection = (HttpURLConnection) url_convert.openConnection();
-        exchangeRate_httpURLConnection.setRequestMethod("GET"); // requesting data
-        int responseCode = exchangeRate_httpURLConnection.getResponseCode();
-
-        if (responseCode == HttpURLConnection.HTTP_OK) // success stabilishing connection
-        {
-            BufferedReader in = new BufferedReader(new InputStreamReader(exchangeRate_httpURLConnection.getInputStream()));
-            String inputLine;
-            StringBuffer response = new StringBuffer();
-
-
-            // add requested data to buffer
-            while ((inputLine = in.readLine()) != null)
-            {
-                response.append(inputLine);
-            }in.close();
-
-            JSONObject obj = new JSONObject(response.toString());
-            exchangeRate = obj.getDouble(currency_from + "_" + currency_to);
-            //System.out.println(exchangeRate);
-
-            double converted_amount = amount * exchangeRate;
-
-            String convert_msg = String.format("Converting %s%.2f (%s) to %s (%s)... \n",
-                    currency_from_symbol,
-                    amount,
-                    currency_from_name,
-                    currency_to,
-                    currency_to_name);
-            String converted_msg = String.format("%s%.2f is equal to %s%.2f.",currency_from_symbol,amount,currency_to_symbol,converted_amount);
-
-            System.out.println(convert_msg);
-            System.out.println(converted_msg);
-
-        }
-
-    }
 
 
 }
