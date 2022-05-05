ba336b2e9ba2e78a163d460a11480d627276d12b
Lucas Kikkawa <45891312+lucaskikkawa@users.noreply.github.com>
2022-05-05 14:00:23 -0300

    Several refactor - change main workflow into methods

--- src/com/company/Converter.java
+++ src/com/company/Converter.java
@@ -5,8 +5,10 @@ import org.json.JSONObject;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigDecimal;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.*;
 
 public class Converter
 {
@@ -98,4 +100,105 @@ public class Converter
 
     }
 
+    public static String validateCurrency(Scanner sc, Map<String, String> currencies){
+        boolean correctCurrency = false;
+        String currency = "";
+
+        while(!correctCurrency)
+        {
+            System.out.println("Please, type in which currency you want to convert FROM:");
+            currency = sc.nextLine().toUpperCase();
+            String check = currencies.get(currency);
+            if (check == null)
+            {
+                System.out.println("Invalid input. Please type a valid currency.");
+            } else
+            {
+                correctCurrency = true;
+            }
+        }
+        return currency;
+    }
+
+    public static void availableCurrencies(Map<String,String> map)
+    {
+        System.out.println("These are my currency converter's available currencies for now.\n");
+        for (Map.Entry<String,String> entry : map.entrySet())
+        {
+            System.out.println(String.format("%s (%s)",entry.getKey(),entry.getValue()));
+        }
+        System.out.println();
+    }
+
+    public static double validateAmount(Scanner sc){
+        boolean correctAmount = false;
+        String amountInput;
+        double amount = 0;
+        BigDecimal checkamount = BigDecimal.valueOf(0);
+        int decimal_digits_amount = 0;
+
+
+        while(!correctAmount)
+        {
+            // Must have <= 2 decimal_digits
+            // 100.00000 <- Double.parseDouble(amountInput) converts to 100.0
+            System.out.println("Please, insert the amount you want to convert using a comma (,) for decimal ( max 2 decimal digits ) : ");
+            amountInput = sc.nextLine();
+            amountInput = amountInput.replaceAll(",",".");
+            if (amountInput.matches("[0-9]+"))
+            {
+                amount = Double.parseDouble(amountInput);
+                checkamount = BigDecimal.valueOf(amount);
+                decimal_digits_amount = checkamount.scale(); // how many decimal digits after "."/
+
+                ;
+                if (amount <= 0 || decimal_digits_amount > 2)
+                {
+                    System.out.println("Invalid input. Please type a valid amount");
+                } else
+                {
+                    correctAmount = true;
+                }
+            } else
+            {
+                System.out.println("Invalid input. Only numbers are allowed.");
+            }
+
+        }
+
+        return amount;
+    }
+
+    public static boolean exitCheck(Scanner sc){
+        boolean exit = false;
+        boolean correctInput = false;
+        String exitInput = "";
+        List<String> validOptions = Arrays.asList("Y","N") ;
+        
+        // check input
+        while(!correctInput)
+        {
+            System.out.println("Do you want to convert again? Type Y for Yes, N for no:");
+            exitInput = sc.nextLine().toUpperCase();
+
+            if (!validOptions.contains(exitInput))
+            {
+                System.out.println("Invalid input. Please type Y or N.");
+            } else
+            {
+                correctInput = true;
+            }
+            
+        }
+
+        if (exitInput.equals(validOptions.get(1))) // No, wants to quit
+        {
+                exit = true;
+        }
+        // no need to check for N input
+
+        return exit;
+    }
 }
+
+
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,9 +7,10 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
-import static com.company.Converter.convert;
+import static com.company.Converter.*;
 
-public class Main {
+public class Main
+{
 
     // I'm going to create a function that takes in original_currency( currency I want to convert ),
     // amount X , destination_currency ( currency I want to convert to )
@@ -31,6 +32,8 @@ public class Main {
         validate amount input (check if it has comma (,) and 2 decimals after comma
         fix exit/repeat flow
 
+        Several refactor - change main parts into methods
+
 
      */
 
@@ -39,32 +42,22 @@ public class Main {
 
         // Add currency code and name
 
-        Map<String,String> currencies = new HashMap<>();
+        Map<String, String> currencies = new HashMap<>();
         currencies.put("BRL", "Brazilian Real");
-        currencies.put("USD","United States Dollar");
-        currencies.put("CAD","Canadian Dollar");
-        currencies.put("AUD","Australian Dollar");
-        currencies.put("JPY","Japanese Yen");
-        currencies.put("ARS","Argentine Peso");
-        currencies.put("CNY","Chinese Yuan");
-        currencies.put("MXN","Mexican Peso");
-        currencies.put("HKD","Hong Kong Dollar");
-        currencies.put("KRW","South Korean Won");
+        currencies.put("USD", "United States Dollar");
+        currencies.put("CAD", "Canadian Dollar");
+        currencies.put("AUD", "Australian Dollar");
+        currencies.put("JPY", "Japanese Yen");
+        currencies.put("ARS", "Argentine Peso");
+        currencies.put("CNY", "Chinese Yuan");
+        currencies.put("MXN", "Mexican Peso");
+        currencies.put("HKD", "Hong Kong Dollar");
+        currencies.put("KRW", "South Korean Won");
 
-        String currency_from = "";
-        String currency_to = "";
-        String exitInput;
-        String amountInput;
-        double amount = 0; // amount >= 0 && amount must have 2 decimal points
-        boolean exit = false;
-        boolean validateCurrencyFrom = false;
-        boolean validateCurrencyTo = false;
-        boolean validateAmount = false;
-        boolean validateExit = false;
-        BigDecimal checkamount = BigDecimal.valueOf(0);
-
-        int decimal_digits_amount = 0;
-
+        String currency_from;
+        String currency_to;
+        double amountUser; // amount >= 0 && amount must have 2 decimal points
+        boolean quit = false;
 
 
         System.out.println("Hello, and welcome to my currency converter, made by Lucas Kikkawa Perpetua.\n");
@@ -73,110 +66,18 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
 
-        while(!exit)
+
+        while (!quit)
         {
-            while(!validateCurrencyFrom)
-            {
-                System.out.println("Please, type in which currency you want to convert FROM:");
-                currency_from = sc.nextLine().toUpperCase();
-                String check = currencies.get(currency_from);
-                if (check == null)
-                {
-                    System.out.println("Invalid input. Please type a valid currency.");
-                } else
-                {
-                    validateCurrencyFrom = true;
-                }
-            }
+            currency_from = validateCurrency(sc, currencies);
+            currency_to = validateCurrency(sc, currencies);
+            amountUser = validateAmount(sc);
 
-            while(!validateCurrencyTo)
-            {
-                System.out.println("Please, type in which currency you want to convert TO:");
-                currency_to = sc.nextLine().toUpperCase();
-                String check = currencies.get(currency_to);
-                if(check == null)
-                {
-                    System.out.println("Invalid input. Please type a valid currency.");
-                } else
-                {
-                    validateCurrencyTo = true;
-                }
-            }
+            convert(currency_from, currency_to, amountUser);
 
-            while(!validateAmount)
-            {
-                // Must have <= 2 decimal_digits
-                // 100.00000 <- Double.parseDouble(amountInput) converts to 100.0
-                System.out.println("Please, insert the amount you want to convert using a comma (,) for decimal ( max 2 decimal digits ) : ");
-                amountInput = sc.nextLine();
-                amountInput = amountInput.replaceAll(",",".");
-                if (amountInput.matches("[0-9]+"))
-                {
-                    amount = Double.parseDouble(amountInput);
-                    checkamount = BigDecimal.valueOf(amount);
-                    decimal_digits_amount = checkamount.scale(); // how many decimal digits after "."/
-
-                    ;
-                    if (amount <= 0 || decimal_digits_amount > 2)
-                    {
-                        System.out.println("Invalid input. Please type a valid amount");
-                    } else
-                    {
-                        validateAmount = true;
-                    }
-                } else
-                {
-                    System.out.println("Invalid input. Only numbers are allowed.");
-                }
-
-            }
-            convert(currency_from, currency_to, amount);
-
-            while(!validateExit)
-            {
-
-                System.out.println("Do you want to convert again? Type Y for Yes, N for no:");
-                exitInput = sc.nextLine().toUpperCase();
-
-                if(!(exitInput.equals("Y") || exitInput.equals("N")))
-                {
-                    System.out.println("Invalid input. Please type Y or N.");
-                }
-                else if (exitInput.equals("Y"))
-                {
-                    // resets while loops
-                    validateCurrencyFrom = false;
-                    validateCurrencyTo = false;
-                    validateAmount = false;
-                    validateExit = true;
-
-                } else if (exitInput.equals("N"))
-                {
-                    exit = true;
-                    validateExit = true;
-                    sc.close();
-                }
-            }
-
-            validateExit = false;
-
+            quit = exitCheck(sc); // quit = true, exits while loop - false, stay in while loop
 
         }
-
         System.out.println("Thanks for using my currency converter! See you soon!");
     }
-
-
-    public static void availableCurrencies(Map<String,String> map)
-    {
-        System.out.println("These are my currency converter's available currencies for now.\n");
-        for (Map.Entry<String,String> entry : map.entrySet())
-        {
-            System.out.println(String.format("%s (%s)",entry.getKey(),entry.getValue()));
-        }
-        System.out.println();
-    }
-
-
-
 }
